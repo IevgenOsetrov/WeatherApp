979beb4447c3e981f7c39e4f1aae10d72eb172de
Evgeniy <jackoset@gmail.com>
2017-10-27 09:23:37 +0300

    Fixed FetchWeather, update api key

--- app/src/main/java/com/dev/sunshine/ForecastFragment.java
+++ app/src/main/java/com/dev/sunshine/ForecastFragment.java
@@ -2,14 +2,11 @@ package com.dev.sunshine;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -24,12 +21,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Locale;
@@ -111,7 +102,7 @@ public class ForecastFragment extends Fragment {
     private void updateWeather() {
         String location = PreferenceManager.getDefaultSharedPreferences(getActivity())
                 .getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
-        new FetchWeatherTask().execute(location);
+        new FetchWeatherTask(getActivity(), mForecastAdapter).execute(location);
     }
 
     /* The date/time conversion code is going to be moved outside the asynctask later,
