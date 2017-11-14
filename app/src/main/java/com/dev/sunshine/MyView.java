87b41e08368fe433d178a7bb284e81ac54be516f
Evgeniy <jackoset@gmail.com>
2017-11-14 17:13:18 +0200

    Custom view

---
+++ app/src/main/java/com/dev/sunshine/MyView.java
@@ -0,0 +1,35 @@
+package com.dev.sunshine;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Evgeniy on 14.11.2017.
+ */
+
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        
+    }
+}
